01590233a88b73efdd6522f0c622acf9cd3ca01d
Prateek <prateek.deshmukh19@gmail.com>
2024-08-04 17:15:13 +0530

    add pixo support

--- bot/bot.py
+++ bot/bot.py
@@ -6,11 +6,18 @@ client = discord.Client(intents=discord.Intents.all())
 
 def uploading(message):
     headersList = {
-        "token": os.getenv('secret'),
+        "token": os.getenv("secret"),
          "Content-Type": "text/plain" 
     }
     url = 'https://live.prateekdeshmukh.com'
     x = requests.post(url, data = message, headers=headersList)
+
+def uploadingImage(img):
+    headersList = {
+        "token": os.getenv("secret2")
+    }
+    url = "https://pixo-81h.pages.dev/api/upload"
+    x = requests.post(url, data = img, headers = headersList)
 
 @client.event
 async def on_ready():
@@ -24,7 +31,11 @@ async def on_message(message):
         print("Message received: ", message.content)
         uploading(message.content)
         return
+    if message.channel.id == 1268065496436178945 and message.attachments:
+        for i in range(len(message.attachments)):
+            content = message.attachments[i].url
+            uploadingImage(content)
+            print(content)
 
 
-client.run(os.getenv('TOKEN'))
-    
+client.run(os.getenv("TOKEN"))
